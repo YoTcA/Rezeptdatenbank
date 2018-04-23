4cd9d408e3dbcd5a26f17398d7a1bbf9d7529148
YoTcA <anonymous1337qwer@posteo.de>
2018-04-23 22:38:37 +0200

    test erfolgreich

--- test/Testsecond.py
+++ test/Testsecond.py
@@ -1,1 +1,7 @@
-print("Hello")
+import tkinter as tk
+class Statusbar(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.test = tk.Button(parent, text="blub")
+        self.test.pack()
--- test/testmain.py
+++ test/testmain.py
@@ -1,20 +1,40 @@
 import tkinter as tk
+import Testsecond
 
 
-class Statusbar(tk.Frame):
-    def __init__(self):
-        super().__init__()
-        self.test = tk.Button(self, text="blub")
+
+class Navbar(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.but1 = tk.Button(parent, text="1")
+        self.but2 = tk.Button(parent, text="2")
+        self.but1.pack(side="left")
+        self.but2.pack(side="left")
+        self.parent.statusbar.test.config(text="bla")
+
+class Window1(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.test = tk.Button(parent, text="blub")
         self.test.pack()
+
+class MainWindow(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.statusbar = Testsecond.Statusbar(self)
+        self.navbar = Navbar(self)
+        self.statusbar.pack()
+        self.navbar.pack()
+
 
 
 class MainApplication(tk.Tk):
-    def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        self.statusbar = Statusbar(self)
+    def __init__(self, *args, **kwargs):
+        super().__init__( *args, **kwargs)
 
-root = MainApplication()
-#scnd = Secondpage(root)
-#button = tk.Button(Mainpage.container, text="hello")
-
-root.mainloop()
+if __name__ == "__main__":
+    root = MainApplication()
+    MainWindow(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
