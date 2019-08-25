d6bbbe82f723a54fefa5f7d2bf1bf80d87ac2c7c
YoTcA <anonymous1337qwer@posteo.de>
2019-08-25 09:09:41 +0200

    pause

--- src/GUI-Test.py
+++ src/GUI-Test.py
@@ -9,24 +9,24 @@ class MainWindow(QMainWindow):
         self.initMe()
 
     def initMe(self):
-        StatusBar = self.statusBar()
-        StatusBar.showMessage("Hello")
+        status_bar = self.statusBar()
+        status_bar.showMessage("Hello")
 
         #Menu File
-        MenuNew = QAction("&New Recipe", self)
-        MenuNew.setShortcut("Ctrl+N")
-        MenuNew.setStatusTip("Create New Recipe")
+        menu_new = QAction("&New Recipe", self)
+        menu_new.setShortcut("Ctrl+N")
+        menu_new.setStatusTip("Create New Recipe")
         #Funktion einfügen!
 
-        MenuExit = QAction("&Exit", self)
-        MenuExit.setShortcut("Ctrl+Q")
-        MenuExit.setStatusTip("Exit Program")
-        MenuExit.triggered.connect(self.close)
+        menu_exit = QAction("&Exit", self)
+        menu_exit.setShortcut("Ctrl+Q")
+        menu_exit.setStatusTip("Exit Program")
+        menu_exit.triggered.connect(self.close)
 
-        MenuBar = self.menuBar()
-        MenuFile = MenuBar.addMenu("&File")
-        MenuFile.addAction(MenuNew)
-        MenuFile.addAction(MenuExit)
+        menu_bar = self.menuBar()
+        menu_file = menu_bar.addMenu("&File")
+        menu_file.addAction(menu_new)
+        menu_file.addAction(menu_exit)
 
         self.setGeometry(50, 50, 500, 500)
         self.setWindowTitle("Rezeptdatenbank V0.1")
@@ -38,15 +38,86 @@ class MainWidget(QWidget):
     def __init__(self, parent):
         super(MainWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
+        top_area = TopWidget(self)
+        self.layout.addWidget(top_area)
+        mid_area = MidWidget(self)
+        self.layout.addWidget(mid_area)
 
-        self.button1 = QPushButton("Button 1")
-        self.layout.addWidget(self.button1)
+class TopWidget(QWidget):
+    def __init__(self, parent):
+        super(TopWidget, self).__init__(parent)
+        self.layout = QHBoxLayout(self)
+        self.setLayout(self.layout)
+        picture = QLabel("Placeholder", self)
+        top_middle = TopMidWidget(self)
+        top_right = TopRightWidget(self)
+        self.layout.addWidget(picture)
+        self.layout.addWidget(top_middle)
+        self.layout.addWidget(top_right)
+        self.layout.addStretch(1)
 
-        self.button2 = QPushButton("Button 2")
-        self.layout.addWidget(self.button2)
-
+class TopMidWidget(QWidget):
+    def __init__(self, parent):
+        super(TopMidWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
         self.setLayout(self.layout)
 
+        # Label to display the duration label
+        duration = QLabel("Duration:", self)
+        self.layout.addWidget(duration)
+
+        # Label to display the effort label
+        effort = QLabel("Effort:", self)
+        self.layout.addWidget(effort)
+
+        # Label to display the rating label
+        rating = QLabel("Rating:", self)
+        self.layout.addWidget(rating)
+
+class TopRightWidget(QWidget):
+    def __init__(self, parent):
+        super(TopRightWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
+
+        # Area to display the duration of the recipe
+        duration = QLineEdit("Duration:", self)
+        duration.setReadOnly(True)
+        self.layout.addWidget(duration)
+
+        # Area to display the effort of the recipe
+        effort = QLineEdit("Effort:", self)
+        effort.setReadOnly(True)
+        self.layout.addWidget(effort)
+
+        # Area to display the rating of the recipe
+        rating = QLineEdit("Rating:", self)
+        rating.setReadOnly(True)
+        self.layout.addWidget(rating)
+
+class MidWidget(QWidget):
+    def __init__(self, parent):
+        super(MidWidget, self).__init__(parent)
+        self.layout = QHBoxLayout(self)
+        self.setLayout(self.layout)
+
+        #Label for number of portions
+        portions = QLabel("Portions:", self)
+        self.layout.addWidget(portions)
+
+        #Choose number of portions
+        portions_select = QDoubleSpinBox(self)
+        self.layout.addWidget(portions_select)
+
+        #Label for tags
+        tags = QLabel("Tags:", self)
+        self.layout.addWidget(tags)
+
+        #Area to display tags
+        tags_display = QLineEdit("Placeholder", self)
+        tags_display.setReadOnly(True)
+        self.layout.addWidget(tags_display)
 
 
 
